c80c22fabbee635899482e9bdbd839d6003f5a27
Ken Fukuyama <kenfdev@gmail.com>
2019-06-13 22:50:59 +0900

    Clean Up
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- components/molecules/RepoResult/index.stories.ts
+++ components/molecules/RepoResult/index.stories.ts
@@ -9,5 +9,45 @@ storiesOf('molecules/RepoResult', module)
   .addDecorator(withKnobs)
   .add('basic', () => ({
     components: { RepoResult },
-    template: `<repo-result />`
+    data() {
+      return {
+        item: {
+          type: 'repository',
+          title: 'remo-exporter',
+          url: 'https://github.com/kenfdev/remo-exporter',
+          description: 'Prometheus Nature Remo Exporter',
+          stats: { language: 'Go', stars: 3, forks: 1 },
+          objectID: '132569661',
+          _highlightResult: {
+            type: {
+              value: '<mark>repo</mark>sitory',
+              matchLevel: 'full',
+              fullyHighlighted: false,
+              matchedWords: ['repo']
+            },
+            title: {
+              value: 'remo-exporter',
+              matchLevel: 'none',
+              matchedWords: []
+            },
+            url: {
+              value: 'https://github.com/kenfdev/remo-exporter',
+              matchLevel: 'none',
+              matchedWords: []
+            },
+            description: {
+              value: 'Prometheus Nature Remo Exporter',
+              matchLevel: 'none',
+              matchedWords: []
+            },
+            stats: {
+              language: { value: 'Go', matchLevel: 'none', matchedWords: [] },
+              stars: { value: '3', matchLevel: 'none', matchedWords: [] },
+              forks: { value: '1', matchLevel: 'none', matchedWords: [] }
+            }
+          }
+        }
+      };
+    },
+    template: `<repo-result :item="item" />`
   }));
