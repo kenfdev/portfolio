6097ee2179d914db1cef66a9c3c3055bd319a3c2
Ken Fukuyama <kenfdev@gmail.com>
2019-06-13 22:50:59 +0900

    Clean Up
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- components/molecules/SlideResult/index.stories.ts
+++ components/molecules/SlideResult/index.stories.ts
@@ -9,5 +9,56 @@ storiesOf('molecules/SlideResult', module)
   .addDecorator(withKnobs)
   .add('basic', () => ({
     components: { SlideResult },
-    template: `<slide-result />`
+    data() {
+      return {
+        item: {
+          type: 'slide',
+          title: 'OpenFaaSとは',
+          thumbnail:
+            'https://speakerd.s3.amazonaws.com/presentations/2a6a0d431c044dcd9253f619be82cc39/preview_slide_0.jpg',
+          url: 'https://speakerdeck.com/kenfdev/openfaastoha',
+          publishedDate: '2017-10-18',
+          description:
+            'Serverless FrameworkなOpenFaaS\nその概要について説明します。\n※Serverless Meetup Osaka #4 のLTにて講演した内容です',
+          objectID: '132569761',
+          _highlightResult: {
+            type: {
+              value: '<mark>slide</mark>',
+              matchLevel: 'full',
+              fullyHighlighted: true,
+              matchedWords: ['slide']
+            },
+            title: {
+              value: 'OpenFaaSとは',
+              matchLevel: 'none',
+              matchedWords: []
+            },
+            thumbnail: {
+              value:
+                'https://speakerd.s3.amazonaws.com/presentations/2a6a0d431c044dcd9253f619be82cc39/preview_<mark>slide</mark>_0.jpg',
+              matchLevel: 'full',
+              fullyHighlighted: false,
+              matchedWords: ['slide']
+            },
+            url: {
+              value: 'https://speakerdeck.com/kenfdev/openfaastoha',
+              matchLevel: 'none',
+              matchedWords: []
+            },
+            publishedDate: {
+              value: '2017-10-18',
+              matchLevel: 'none',
+              matchedWords: []
+            },
+            description: {
+              value:
+                'Serverless FrameworkなOpenFaaS\nその概要について説明します。\n※Serverless Meetup Osaka #4 のLTにて講演した内容です',
+              matchLevel: 'none',
+              matchedWords: []
+            }
+          }
+        }
+      };
+    },
+    template: `<slide-result :item="item" />`
   }));
