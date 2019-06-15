29acc7ce439c865e46f151280470303c8cce3ae1
Ken Fukuyama <kenfdev@gmail.com>
2019-06-15 11:58:43 +0900

    Change viewport meta tag
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,11 @@ const config: NuxtConfiguration = {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1'
+      },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
