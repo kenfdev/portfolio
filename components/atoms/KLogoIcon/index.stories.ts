23f1c647ee1453ffac5fa3b9a84f293b825a943a
Ken Fukuyama <kenfdev@gmail.com>
2021-04-03 09:29:58 +0900

    clean

--- components/atoms/KLogoIcon/index.stories.ts
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-
-import KLogoIcon from './index.vue';
-
-storiesOf('atoms/KLogoIcon', module).add('basic', () => ({
-  components: { KLogoIcon },
-  template: `<k-logo-icon icon="fa-github" url="https://github.com/kenfdev" />`
-}));
--- components/templates/TopTemplate/index.stories.ts
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-
-import TopTemplate from './index.vue';
-
-storiesOf('templates/TopTemplate', module).add('basic', () => ({
-  components: { TopTemplate },
-  template: `<top-template />`
-}));
