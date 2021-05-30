641861a82d596da452192292384bcc23055b21c5
Ken Fukuyama <kenfdev@gmail.com>
2021-05-31 07:16:21 +0900

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-4PYHHZYCHB',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
