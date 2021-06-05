d5f1ff86bd8b1bc60508c5fa7d27c7d832f2fdf2
Ken Fukuyama <kenfdev@gmail.com>
2021-06-05 09:23:01 +0900

    improve seo

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useLocation } from '@reach/router';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default function Layout({ children }) {
+  const { pathname } = useLocation();
+  const { site } = useStaticQuery(query);
+  const { description, siteUrl, title, twitterUsername, defaultImage } =
+    site.siteMetadata;
+  const seo = {
+    title: title,
+    description: description,
+    image: `${siteUrl}${defaultImage}`,
+    url: `${siteUrl}${pathname}`,
+  };
+
   return (
     <div>
-      <Helmet>
+      <Helmet title={seo.title}>
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
+        {seo.url && <meta property="og:url" content={seo.url} />}
+        {seo.title && <meta property="og:title" content={seo.title} />}
+        {seo.description && (
+          <meta property="og:description" content={seo.description} />
+        )}
+        {seo.image && <meta property="og:image" content={seo.image} />}
+        <meta name="twitter:card" content="summary_large_image" />
+        {twitterUsername && (
+          <meta name="twitter:creator" content={twitterUsername} />
+        )}
+        {seo.title && <meta name="twitter:title" content={seo.title} />}
+        {seo.description && (
+          <meta name="twitter:description" content={seo.description} />
+        )}
+        {seo.image && <meta name="twitter:image" content={seo.image} />}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap"
@@ -15,3 +45,17 @@ export default function Layout({ children }) {
     </div>
   );
 }
+
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        description
+        siteUrl
+        title
+        twitterUsername
+        defaultImage
+      }
+    }
+  }
+`;
